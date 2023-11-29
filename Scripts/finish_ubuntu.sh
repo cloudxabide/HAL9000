7e0cca524e7037289f846cebb4dadbc654fd641e
James Radtke <cloudxabide@gmail.com>
2023-11-29 13:44:43 -0500

    Adding PC Ubuntu Host

---
+++ Scripts/finish_ubuntu.sh
@@ -0,0 +1,203 @@
+#!/bin/bash
+
+# Purpose:  To update my base install to run AI/ML workloads on my Dell XPS15 or Asus PC
+#  Status:  In-progress - trying to convert this to run as a non-root user
+
+# * * * * * * * * * * * *
+# System Stuff
+# * * * * * * * * * * * *
+PWD=`pwd`
+DATE=`date +%Y%m%d`
+ARCH=`uname -p`
+PKG_MGR=$(which apt || which apt-get)
+
+# Make sure the host is named correctly
+case $(dmidecode  -s baseboard-product-name) in
+  21CD000QUS)
+    MYHOSTNAME="blackmesa"
+  ;;
+  0F6K9V)
+    MYHOSTNAME="slippy" 
+  ;;
+  'ROG STRIX Z490-E GAMING')
+    MYHOSTNAME="hal-9000" 
+  ;;
+esac
+echo "$MYHOSTNAME" | sudo tee /etc/hostname
+
+# Customize environment for Morpheus
+# Add TMPFS mount for user:morpheus
+grep morpheus /etc/fstab
+if [ $? -ne 0 ]
+then
+  sudo cp /etc/fstab /etc/fstab.`date +%F`
+  echo "# TMPFS Mount" | sudo tee -a /etc/fstab
+  echo "tmpfs   /home/morpheus tmpfs  rw,size=1G,nr_inodes=5k,noexec,nodev,nosuid,uid=2026,gid=2026,mode=1700   0  0" | sudo tee -a /etc/fstab
+  sudo mkdir /home/morpheus
+fi
+
+sudo systemctl daemon-reload
+sudo mount -a
+
+# Install "Standard OS Packages"
+SYSTEM_PACKAGES="wget gpg curl git npm"
+sudo apt-get -y install $SYSTEM_PACKAGES
+
+# Manage Users 
+id -u jradtke &>/dev/null || sudo useradd -m -u2025 -G10 -c "James Radtke" -p '$6$MIxbq9WNh2oCmaqT$10PxCiJVStBELFM.AKTV3RqRUmqGryrpIStH5wl6YNpAtaQw.Nc/lkk0FT9RdnKlEJEuB81af6GWoBnPFKqIh.' -s /bin/bash jradtke
+id -u morpheus &>/dev/null || sudo useradd -m -u2026 -c "Morpheus" -p '$6$MIxbq9WNh2oCmaqT$10PxCiJVStBELFM.AKTV3RqRUmqGryrpIStH5wl6YNpAtaQw.Nc/lkk0FT9RdnKlEJEuB81af6GWoBnPFKqIh.' morpheus
+
+# SUDO
+SUDOERS="jradtke mansible"
+for USER in $SUDOERS
+do
+  [ ! -f /etc/sudoers.d/$USER-nopasswd-all ] && echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$USER-nopasswd-all
+done
+
+
+# Install Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+echo "deb https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
+sudo apt-get update -y
+sudo apt-get install -y google-chrome-stable
+
+# Install VSCode (this mess of code is from Microsoft - I will leave it as-is)
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt update
+sudo apt install -y apt-transport-https code # or code-insiders
+
+# Prevent screen hang - for Dell (need to add a case statement here)
+case $(dmidecode  -s baseboard-product-name) in
+  21CD000QUS)
+cat << EOF | sudo tee /etc/modprobe.d/i915.conf
+options i915 enable_psr=0
+EOF
+;;
+esac
+
+# Update User Shell Environment
+mkdir ${HOME}/.bashrc.d/
+cd
+for FILE in .bash_profile .bashrc .gitconfig .gitignore .gitignore_global
+do
+  echo "curl -o ${FILE} https://raw.githubusercontent.com/cloudxabide/devops/main/Files/${FILE}"
+  curl -o ${FILE} https://raw.githubusercontent.com/cloudxabide/devops/main/Files/${FILE}
+done
+
+curl -o ${HOME}/.bashrc.d/common https://raw.githubusercontent.com/cloudxabide/devops/main/Files/.bashrc.d_common
+curl -o ${HOME}/.bashrc.d/ubuntu https://raw.githubusercontent.com/cloudxabide/devops/main/Files/.bashrc.d_ubuntu
+
+# Manage Login Screen (remove some users)
+USERS="mansible morpheus sharriradtke sophos techies"
+for USER in $USERS
+do
+echo "[User]
+Language=en_US.UTF-8
+XSession=gnome
+SystemAccount=true" | sudo tee /var/lib/AccountsService/users/$USER
+done
+
+# Update GRUB menu
+wget -O - https://github.com/shvchk/fallout-grub-theme/raw/master/install.sh | sudo bash -
+
+# CUDA / Docker stuff
+## First - remove existing NVIDIA bits
+sudo apt autoremove -y nvidia* --purge
+sudo /usr/bin/nvidia-uninstall
+
+ubuntu-drivers devices
+apt install -y nvidia-driver-535 && shutdown now -r
+
+lspci | egrep -i 'vga|3d|display'
+lsmod | grep nvidia
+nvidia-smi
+# glxgears
+
+# Install Docker CE
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y 
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update -y && sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker
+
+# Note:  you have to logout/login for the usermod to take effect
+sudo usermod -aG docker $(whoami)
+
+# Run a test to see
+docker run --rm --gpus all nvidia/cuda:12.0.0-base-ubuntu22.04 nvidia-smi
+
+#  Update the system (and reboot)
+sudo apt -y update
+sudo apt -y upgrade
+sudo reboot
+
+# Ollama stuff here
+# https://ollama.ai/ => https://github.com/jmorganca/ollama
+# https://hub.docker.com/r/ollama/ollama
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
+    | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
+    | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
+    | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+
+# I don't believe this is necessary
+# sudo nvidia-ctk runtime configure --runtime=docker
+# sudo systemctl restart docker
+
+#docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+OLLAMA_ENV_FILE=${HOME}/ollama-env-file.txt
+echo "OLLAMA_ORIGINS=http://10.10.10.20:*,http://hal-9000.linuxrevolution.com:*" > $OLLAMA_ENV_FILE
+docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --env-file=$OLLAMA_ENV_FILE --name ollama ollama/ollama
+
+# CLI interface
+docker exec -it ollama ollama run llama2
+
+exit 0
+
+###### ###### 
+#  Random TESTING FOLLOWS...
+###### ###### 
+# OLLaMa 
+# https://github.com/ollama-webui/ollama-webui
+#docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway --name ollama-webui --restart always ghcr.io/ollama-webui/ollama-webui:main
+docker run -d -p 3000:8080 -e OLLAMA_API_BASE_URL=http://10.10.10.20:11434/api --name ollama-webui --restart always ghcr.io/ollama-webui/ollama-webui:main
+
+cd
+git clone https://github.com/ollama-ui/ollama-ui
+cd ollama-ui
+make
+sudo ufw allow port 8000
+# Now open the following:  http://localhost:8000 # in browser
+
+# API test direct to host
+curl http://10.10.10.20:11434/api/generate -d '{
+  "model": "llama2",
+  "prompt": "[INST] why is the sky blue? [/INST]",
+  "raw": true,
+  "stream": false
+}'
+
+# Kill/Remove the pods
+docker kill $(docker ps -a | awk '{ print $1 }' | grep -v CONTAINER)
+docker rm $(docker ps -a | awk '{ print $1 }' | grep -v CONTAINER)
+
---
+++ Ubuntu.md
@@ -0,0 +1,5 @@
+# How to Build my Ubuntu Workstation
+
+
+
+
